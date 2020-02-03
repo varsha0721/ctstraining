9fb16b4ef0934910d564793d85f9536fed7accfe
Alchemy <Alchemy@192.168.1.156>
2020-02-03 17:22:21 +0530

    31_01_2020

---
+++ form-controls1/src/main/java/comm/example/spring/controller/Student.java
@@ -0,0 +1,83 @@
+package comm.example.spring.controller;
+
+import java.util.LinkedHashMap;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Range;
+
+public class Student {
+
+	@NotNull
+	@Size(min = 5,max = 20,message = "first name required")
+	private String firstName;
+	@NotNull
+	@Size(min = 5,max = 20,message = "last name required")
+	private String lastName;
+	private String country;
+	private LinkedHashMap<String, String> countryOptions;
+	
+	private String favoriteLanguage;
+	
+	private String[] operatingSystems;
+	
+	public Student() {
+		
+		// populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<>();
+		
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("US", "United States of America");		
+
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
+	}
+			
+}
---
+++ form-controls1/src/main/java/comm/example/spring/controller/StudentController.java
@@ -0,0 +1,64 @@
+package comm.example.spring.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+
+	@GetMapping("/showForm")
+	public String showForm(Model theModel) {
+		
+		// create a student object
+		Student theStudent = new Student();
+		
+		// add student object to the model
+		theModel.addAttribute("student", theStudent);
+		
+		return "student-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(@Valid@ModelAttribute("student") Student theStudent,BindingResult theResult) {
+		
+		// log the input data
+		System.out.println("theStudent: " + theStudent.getFirstName()
+							+ " " + theStudent.getLastName());
+		
+		if(theResult.hasErrors())
+		{
+			return "student-form";
+		}
+		else {
+			return "student-confirmation";
+		}
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
